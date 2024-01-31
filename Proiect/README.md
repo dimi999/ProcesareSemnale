f7d76e03dcd92c40ee6b9ca21096e22416a49eae
dimi999 <dimitriu.andrei@yahoo.com>
2024-01-31 19:43:19 +0200

    Reorganize code

---
+++ Proiect/README.md
@@ -0,0 +1,13 @@
+# Detectarea liniilor din imagini folosind Transformata Hough
+
+Documentatia proiectului se afla in fisierul PDF.
+
+Codul pentru detectia liniilor, inclusiv pentru tunarea parametrilor, se afla in fisierele .py.
+
+Codul care genereaza graficele din documentatie se afla in cele doua fisiere .ipynb.
+
+Echipa:
+- Messner Annemarie Beatrix (grupa 351)
+- Dimitriu Andrei Dragos (grupa 352)
+- Gheorghe Liviu Armand (grupa 352)
+- Oprea Mihai-Adrian (grupa 352)
--- Proiect/main.py
+++ Proiect/main.py
@@ -3,6 +3,65 @@ import numpy as np
 import cv2
 from PIL import Image
 
+
+#https://gist.github.com/kylemcdonald/bedcc053db0e7843ef95c531957cb90f
+def full_frame(width=None, height=None):
+    import matplotlib as mpl
+    mpl.rcParams['savefig.pad_inches'] = 0
+    figsize = None if width is None else (width, height)
+    fig = plt.figure(figsize=figsize)
+    ax = plt.axes([0,0,1,1], frameon=False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.autoscale(tight=True)
+    
+def get_coord(th, ro):
+    if abs(np.sin(th)) < 1e-2:
+        return (ro / np.cos(th), 'vertical_line')
+    if abs(np.cos(th)) < 1e-2:
+        m = 0.
+    else:
+        m = - 1 / np.tan(th)   
+    c = ro / np.sin(th)
+    
+    return (m, c)
+    
+def draw_line(m, c):
+    if c == 'vertical_line':
+        if m < 0:
+            return
+        samples = [m for i in range(100)]
+        values = np.linspace(1, height - 1, 100)
+        plt.plot(samples, values, c='black', linewidth=.5)
+        return
+    samples = np.linspace(0, width, 100)
+    samples = samples[height - (samples * m + c) <= height]
+    samples = samples[height - (samples * m + c) >= 0]
+    values = height - (m * samples + c)
+    if len(samples) > 1:
+        plt.plot(samples, values, c='black', linewidth=.5)
+        
+def score_compute(img1, img2):
+    correct, incorrect, missing = 0, 0, 0
+    global img5
+    for i in range(height):
+        for j in range(width):
+            if img1[i,j] != img2[i,j]:
+                img5[i,j] = abs(img1[i,j] - img2[i,j])
+                if img5[i,j] <= 20 and img2[i,j] <= 50: 
+                    #acceptable differnece between blacks
+                    correct += 1
+                elif img1[i, j] <= 30: # wrong line
+                    incorrect += 1
+                elif img2[i,j] <= 30: # missing line
+                    missing += 1 
+    if correct == 0 and (incorrect == 0 or missing ==0):
+        return 0 #image is empty
+    
+    return correct / (missing * incorrect)
+
+    
+
 imgs = ['test_stea.png']#['test_linii.png', 'test_sah.png', 'test_triunghi.png', 'test_concav.png', 'test_stea.png']
 correct_imgs = ['test_stea_corect.png']#['test_linii_corect.png', 'test_sah_corect.png', 'test_triunghi_corect.png', 'test_concav_corect.png', 'test_stea_corect.png']
 
@@ -80,49 +139,10 @@ for it_gradient in range(len(gradient_threshold)):
 #print(Accumulator[Accumulator>=100])
             #%%
             threshold_vote = voting_threshold[it_vot] 
-            
-            #https://gist.github.com/kylemcdonald/bedcc053db0e7843ef95c531957cb90f
-            def full_frame(width=None, height=None):
-                import matplotlib as mpl
-                mpl.rcParams['savefig.pad_inches'] = 0
-                figsize = None if width is None else (width, height)
-                fig = plt.figure(figsize=figsize)
-                ax = plt.axes([0,0,1,1], frameon=False)
-                ax.get_xaxis().set_visible(False)
-                ax.get_yaxis().set_visible(False)
-                plt.autoscale(tight=True)
-                
-                
-                
+        
             full_frame()
             plt.imshow(img, cmap='gray', alpha=1)
             
-            def get_coord(th, ro):
-                if abs(np.sin(th)) < 1e-2:
-                    return (ro / np.cos(th), 'vertical_line')
-                if abs(np.cos(th)) < 1e-2:
-                    m = 0.
-                else:
-                    m = - 1 / np.tan(th)   
-                c = ro / np.sin(th)
-                
-                return (m, c)
-                
-            def draw_line(m, c):
-                if c == 'vertical_line':
-                    if m < 0:
-                        return
-                    samples = [m for i in range(100)]
-                    values = np.linspace(1, height - 1, 100)
-                    plt.plot(samples, values, c='red', linewidth=.5)
-                    return
-                samples = np.linspace(0, width, 100)
-                samples = samples[height - (samples * m + c) <= height]
-                samples = samples[height - (samples * m + c) >= 0]
-                values = height - (m * samples + c)
-                if len(samples) > 1:
-                    plt.plot(samples, values, c='red', linewidth=.5)
-                    pass
             for i in range(Accumulator.shape[0]):
                 for j in range(Accumulator.shape[1]):
                     if Accumulator[i,j] > threshold_vote:
@@ -147,24 +167,6 @@ for it_gradient in range(len(gradient_threshold)):
             
             img5 = np.zeros((height, width))
             
-            def score_compute(img1, img2):
-                correct, incorrect, missing = 0, 0, 0
-                global img5
-                for i in range(height):
-                    for j in range(width):
-                        if img1[i,j] != img2[i,j]:
-                            img5[i,j] = abs(img1[i,j] - img2[i,j])
-                            if img5[i,j] <= 20 and img2[i,j] <= 50: 
-                                #acceptable differnece between blacks
-                                correct += 1
-                            elif img1[i, j] <= 30: # wrong line
-                                incorrect += 1
-                            elif img2[i,j] <= 30: # missing line
-                                missing += 1 
-                if correct == 0 and (incorrect == 0 or missing ==0):
-                    return 0 #image is empty
-                
-                return correct / (missing * incorrect)
             score += (score_compute(img2, img3)) / len(imgs)
             
             #plt.imshow(img5)
